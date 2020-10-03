e9e13b3708f5db79bc9b1894a99379b770d8c78b
sarthak2404 <sarthakgulati24@gmail.com>
2020-10-03 16:16:59 +0530

    all the files are uploaded

---
+++ script.js
@@ -0,0 +1,46 @@
+var css = document.querySelector("h3");
+
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("grad");
+
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+    css.textContent = body.style.background + ";";
+    
+}
+
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
+var fancyHeading = document.getElementsByClassName("fanc")[0];
+var letters = fancyHeading.textContent.split('');
+
+var content = letters.map((val, i) => {
+  let delay = Math.floor((Math.random() * 1000) + 1);
+  return ('<span style="animation-delay: '+ delay + 'ms">'
+          + val +
+          '</span>');
+});
+
+fancyHeading.innerHTML = "";
+
+for (var i = 0; i < content.length; i++) {
+  fancyHeading.innerHTML += content[i];
+}
+
+function copyToClipboard(element) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    $temp.val($(element).text()).select();
+    document.execCommand("copy");
+    $temp.remove();
+  }
+  
